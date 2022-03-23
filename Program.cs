c0411709a30dcdb8e9725e3c026834b144926e36
naufalrbni <naufalrbbnii@gmail.com>
2022-03-23 20:54:28 +0700

    inisialisasi projek

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // penentuan nilai properties
+            taxi.DriverName = "Joko";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
